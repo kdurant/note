c3737f0ad76f71cdf3738a80798842f339eb9480
kdurant <wangjun850725@163.com>
2019-01-25 15:47:48 +0800

    实现插入排序功能

--- Algorithms/sort/insert_sort.c
+++ Algorithms/sort/insert_sort.c
@@ -5,8 +5,8 @@ void insert_sort(int *nums, int len)
     int temp;
     for(size_t i = 1; i < len; i++)
     {
-        for(size_t j = 1; j < i+1; j++)
-        {
+        for(size_t j = i; j > 0; j--)
+        { 
             if(nums[j] < nums[j-1] )  // 后一个元素小于前一个元素
             {
                 temp = nums[j];
@@ -21,9 +21,9 @@ void insert_sort(int *nums, int len)
 
 int main()
 {
-    int nums[5] = {44, 38, 5, 3, 47};
-    insert_sort(nums, 5);
-    for(size_t i = 0; i < 5; i++)
+    int nums[6] = {44, 38, 5, 3, 47, 50};
+    insert_sort(nums, 6);
+    for(size_t i = 0; i < 6; i++)
     {
         printf("%d ", nums[i]);
     }
