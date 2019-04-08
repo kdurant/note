bb65b882240a712872da4f372081c70a83130102
kdurant_work <wangjun850725@163.com>
2019-04-08 08:32:55 +0800

    sync

--- leetcode/main.c
+++ leetcode/main.c
@@ -6,42 +6,20 @@
 
 #include<limits.h>
 
-int divide(int dividend, int divisor) {
-    int num = 0;
-    int sign = 0;
+struct test
+{
+	char data[4];
+};
 
-    if(dividend >= 0 && divisor > 0)
-        sign = 1;
-    else if(dividend <= 0 && divisor < 0)
-        sign = 1;
-    else 
-        sign = 0;
-
-    dividend = dividend >= 0 ? dividend : 0 - dividend;
-    divisor = divisor > 0 ? divisor : 0 - divisor;
-
-
-    while( dividend >= divisor)
-    {
-        dividend -= divisor;
-        num = num + 1;
-    }
-    if (sign == 0)
-        num = INT_MIN + (INT_MIN - num);
-    return num;
-}
+struct test t =
+{
+	.data = "2.00"
+};
 
 int main()
 {
-    // int nums[] = {0,11,0,3};
-    char nums1[] = {'a', 'b', 'c', 'd'};
-    printf("%d\n", INT_MAX);
-    printf("%d\n", INT_MIN);
-    printf("%d\n", divide(10, 3));
-    printf("%d\n", divide(10, -3));
-    printf("%d\n", divide(-10, 3));
-    printf("%d\n", divide(-10, -3));
-    printf("%d\n", divide(-1, 1));
-    printf("%d\n", divide(-2147483648, -1));
+	struct test *p = &t;
+	p->data[0] = '4';
+    printf("%s\n", t.data);
     return 0;
 } 
