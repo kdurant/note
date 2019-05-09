fc53845ad0e69b2d16a19515a5e6f4ffe1e52803
kdurant_work <wangjun850725@163.com>
2019-05-09 22:21:14 +0800

    sync

--- Algorithms/sort/sort.c
+++ Algorithms/sort/sort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * @brief 冒泡排序
@@ -83,4 +84,81 @@ void insert_sort(int *data, int len)
         }
         
     }
+}
+
+/**
+ * @brief 归并排序
+ * 
+ * @param data 
+ * @param first 
+ * @param last 
+ */
+
+void merge(int *data, int low, int mid, int high)
+{
+    int i, k;
+    int *tmp = malloc((high-low+1)*sizeof(int));
+    int left_low = low;
+    int left_high = mid;
+    int right_low = mid + 1;
+    int right_high = high;
+    for (k = 0; left_low <= left_high && right_low <= right_high; k++)
+    {
+        if(data[left_low] <= data[right_low])
+        {
+            tmp[k] = data[left_low++];
+        }
+        else
+        {
+            tmp[k] = data[right_low++];
+        }  
+    }
+
+    if(left_low <= left_high)
+    {
+        for (size_t i = left_low; i <= left_high; i++)
+        {
+            tmp[k++] = data[i];
+        }
+    } 
+
+    if(right_low <= right_high)
+    {
+        for (size_t i = right_low; i <= right_high; i++)
+        {
+            tmp[k++] = data[i];
+        }
+    } 
+    for (size_t i = 0; i < high-low+1; i++)
+    {
+        data[low+i] = tmp[i];
+    }
+    free(tmp);
+    return; 
+}
+
+void merge_sort(int *data, unsigned int first, unsigned int last)
+{
+    int mid = 0;
+    if(first < last)
+    {
+        mid = (first + last) / 2;
+        merge_sort(data, first, mid);
+        merge_sort(data, mid + 1, last);
+        merge(data, first, mid, last);
+    }
+}
+
+
+int main()
+{
+    // int a[] = {900, 2, 3, -58, 34, 76, 32, 43, 56, -70, 35, -234, 532, 543, 2500};
+    int a[] = {900, 2, 3, -58};
+
+    merge_sort(a, 0, sizeof(a)/4-1);
+    for (size_t i = 0; i < sizeof(a)/4; i++)
+    {
+        printf("%d\t", a[i]);
+    }
+    
 }
