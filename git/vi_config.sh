41f17a78470eff820ca8fbaa610d215b37f692ee
kdurant <wangjun850725@163.com>
2019-11-23 17:06:00 +0800

    sync

---
+++ git/vi_config.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+vim_config_folder='vimrc'
+
+ssh_config_folder="/home/wj/.ssh"
+
+if [ -d "$vim_config_folder" ]; then
+    cd $vim_config_folder
+    cp .config/init.vim ~/.config/nvim
+    cp .config/nvim/basic_map.vim ~/.config/nvim
+    cp .config/nvim/plugin_config.vim ~/.config/nvim
+    cp .config/nvim/plugin_map.vim ~/.config/nvim
+    cp .config/nvim/self_fun.vim ~/.config/nvim
+    cp .config/nvim/user_plugin.vim ~/.config/nvim
+
+    if [ ! -d "$ssh_config_folder" ]; then
+        mkdir $ssh_config_folder
+        cp .ssh/id_rsa $ssh_config_folder
+        cp .ssh/id_rsa.pub $ssh_config_folder
+    fi
+else
+    git clone https://github.com/kdurant/vimrc
+fi
+
+#git clone https://github.com/kdurant/bookmark
+
