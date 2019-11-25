75624b7f73508a3cb1d6bf58ca8238b6152d4213
kdurant <wangjun850725@163.com>
2019-11-25 13:56:21 +0800

    sync

--- git/vi_config.sh
+++ git/vi_config.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 vim_config_folder='vimrc'
 
 ssh_config_folder="/home/wj/.ssh"
